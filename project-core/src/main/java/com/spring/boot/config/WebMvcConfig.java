b710d6ba52c6acdd3147dad4be66630f1b433c80
yangTobey <1248079389@qq.com>
2018-12-06 00:10:18 +0800

    版本

--- project-core/src/main/java/com/spring/boot/config/WebMvcConfig.java
+++ project-core/src/main/java/com/spring/boot/config/WebMvcConfig.java
@@ -1,8 +1,12 @@
 package com.spring.boot.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -10,6 +14,9 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/1/25.
@@ -34,6 +41,22 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.favorPathExtension(false);
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        converter.setFeatures(SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullBooleanAsFalse);
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.parseMediaType("text/plain;charset=utf-8"));
+        mediaTypes.add(MediaType.parseMediaType("text/html;charset=utf-8"));
+        mediaTypes.add(MediaType.parseMediaType("text/json;charset=utf-8"));
+        mediaTypes.add(MediaType.parseMediaType("application/json;charset=utf-8"));
+        //mediaTypes.add(MediaType.parseMediaType("application/x-www-form-urlencoded"));
+        converter.setSupportedMediaTypes(mediaTypes);
+        converters.add(converter);
     }
     /**
      * 允许CORS
