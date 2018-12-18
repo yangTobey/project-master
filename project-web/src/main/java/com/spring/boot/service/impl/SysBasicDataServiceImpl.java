1f5e3f26a1e90df15970f55444568a42d02de648
yangTobey <1248079389@qq.com>
2018-12-18 22:44:21 +0800

    分页

--- project-web/src/main/java/com/spring/boot/service/impl/SysBasicDataServiceImpl.java
+++ project-web/src/main/java/com/spring/boot/service/impl/SysBasicDataServiceImpl.java
@@ -1,5 +1,6 @@
 package com.spring.boot.service.impl;
 
+import com.github.pagehelper.PageHelper;
 import com.spring.boot.bean.master.SysBasicData;
 import com.spring.boot.bean.master.SysBasicDataFile;
 import com.spring.boot.bean.master.SysQualityManageFile;
@@ -117,6 +118,10 @@ public class SysBasicDataServiceImpl implements SysBasicDataService {
             map.put("limit", limit);
             map.put("offset", offset);
             map.put("year", year);
+            //设置分页信息，分别是当前页数和每页显示的总记录数【记住：必须在mapper接口中的方法执行之前设置该分页信息】
+            PageHelper.startPage(1,10,"create_time desc");
+            PageHelper.orderBy("desc");
+            sysBasicDataBusinessService.sysBasicDataAnalysisList(map);
             resultMap.put("total", sysBasicDataBusinessService.sysBasicDataAnalysisListTotal(map));
             resultMap.put("list", sysBasicDataBusinessService.sysBasicDataAnalysisList(map));
             return R.ok().putData(200, resultMap, "获取成功！");
